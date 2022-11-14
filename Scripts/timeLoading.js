b5e685f11208ca3b6a41aa3402550a11679e8565
Dolphin-in-river <311508@niuitmo.ru>
2022-11-14 15:30:01 +0300

    fix: change method for calculate time load

--- Scripts/timeLoading.js
+++ Scripts/timeLoading.js
@@ -1,10 +1,9 @@
 (function () {
-    let open_time = new Date().getTime();
+    let open_time = window.performance.timing.domLoading - window.performance.timing.navigationStart;
 
     function load_time() {
         document.getElementById('load-time').innerHTML =
-            'Время загузки составило: ' + (new Date().getTime() - open_time) / 1000 + ' сек';
+            'Время загузки составило: ' + open_time / 1000 + ' сек';
     }
-
     document.addEventListener('DOMContentLoaded', load_time);
 })();
