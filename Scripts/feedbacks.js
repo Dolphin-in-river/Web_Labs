14e46376d0de364e96d78ee68ae84b0b158758bd
Dolphin-in-river <311508@niuitmo.ru>
2022-11-28 10:40:08 +0300

    fix: change regex match

--- Scripts/feedbacks.js
+++ Scripts/feedbacks.js
@@ -21,7 +21,7 @@ function listenToSubmit() {
             return
         }
 
-        if (!feedbackContent.match(/^[a-zA-Zа-яА-Я0-9\s]*$/)) {
+        if (!feedbackContent.match(/^[a-zA-Zа-яА-Я0-9\s!?\.]*$/)) {
             alert("Разрешены только буквы и цифры!")
             return
         }
