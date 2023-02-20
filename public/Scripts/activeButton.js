72704fe01d47b7b7401da30646812c74ed9244f8
Dolphin-in-river <311508@niuitmo.ru>
2023-02-21 00:03:54 +0300

    add static frontend

---
+++ public/Scripts/activeButton.js
@@ -0,0 +1,10 @@
+(function () {
+
+    function pageIsActive() {
+        let itemHtml = document.location.pathname.split('/').pop();
+        let item = itemHtml.substring(0, itemHtml.length - 5)
+        document.getElementById(item).style.textDecoration = 'underline';
+    }
+
+    document.addEventListener('DOMContentLoaded', pageIsActive);
+})();
